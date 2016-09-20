dc3db08a812eee7941cfe9e4402ef1d030455126
Patrick Kilter <pas.freelance@gmail.com>
2016-09-20 23:16:05 +0200

    prototyped .zip

---
+++ src/gobu/zip.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+"archive/zip"
+"io"
+"log"
+"os"
+"path/filepath"
+"strings"
+)
+
+func untar(source, target string) {
+
+r, err := zip.OpenReader(source)
+
+if err != nil {
+        log.Fatal(err)
+}
+
+defer r.Close()
+
+for _, f := range r.File {
+
+        fmt.Printf("%v", f)
+}
+
+
+}
