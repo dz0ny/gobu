41691402cfa6d4bd6b965f198746cdf4339f5eb9
Patrick Simon <pas.freelance@googlemail.com>
2016-09-21 00:01:15 +0200

    unzipping

--- src/gobu/zip.go
+++ src/gobu/zip.go
@@ -6,22 +6,51 @@ import (
 "log"
 "os"
 "path/filepath"
-"strings"
 )
 
-func zip(source, target string) {
+func unzip(source, target string) {
 
 	r, err := zip.OpenReader(source)
+
+	defer r.Close()
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer r.Close()
+	if err := os.MkdirAll(target, 0755); err != nil {
 
-	for _, f := range r.File {
+		log.Fatal(err)
+	}
 
-		fmt.Printf("%v", f)
+
+	for _, file := range r.File {
+
+		path := filepath.Join(target, file.Name)
+		if file.FileInfo().IsDir() {
+			os.MkdirAll(path, file.Mode())
+			continue
+		}
+
+		fileReader, err := file.Open()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer fileReader.Close()
+
+		targetFile, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, file.Mode())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer targetFile.Close()
+
+		if _, err := io.Copy(targetFile, fileReader); err != nil {
+
+			log.Fatal(err)
+		}
+
 	}
 
 
